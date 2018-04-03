5f9041d4a0354c7c74c8ab048d21324db48eef5a
atholt95 <32171853+atholt95@users.noreply.github.com>
2018-04-02 22:44:54 -0500

    FirstPush
    FirstPush

---
+++ SummerHorrorGame/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    
+    public Animator anim;
+    public AudioSource doorOpen;
+    public AudioSource doorClose;
+    public bool doorIsOpen;
+    public bool doorIsClosed ;
+
+	// Use this for initialization
+	void Start () {
+        doorIsOpen = false;
+        doorIsClosed = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+    private void OnTriggerStay(Collider player)
+    {
+        if (player.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            if (doorIsClosed)
+            {
+                anim.Play("DoorOpen");
+                doorOpen.Play();
+                doorIsOpen = true;
+                doorIsClosed = false;
+                
+            }
+            if (doorIsOpen)
+            {
+                anim.Play("DoorClose");
+                doorClose.Play();
+                doorIsClosed = true;
+                doorIsOpen = false;
+            }
+            
+        }
+       
+    }
+}
